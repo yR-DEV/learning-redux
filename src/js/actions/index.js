17637459fa80a240a80607429bc5dee6b0ca1f29
yeah-right <claar.zack@gmail.com>
2019-06-05 11:12:34 -0600

    Added actions and constants. Going to refactor the reducer now

---
+++ src/js/actions/index.js
@@ -0,0 +1,6 @@
+import { ADD_POST } from '../constants/action-types';
+
+export function addPost(payload) {
+    return { type: "ADD_POST", payload }
+};
+
--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,5 +1,5 @@
 const initialState = {
-    articles: []
+    posts: []
 };
 
 rootReducer = (state = initialState, action) => {
